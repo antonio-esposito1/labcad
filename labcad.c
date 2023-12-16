142a3f238671437742acf95ec6554bfaa0e56971
Antonio Esposito <antonio.esposito1@inwind.it>
2023-12-16 21:17:33 +0100

    uso degli operatori di membro di struttura e di puntatore a struttura

--- labcad.c
+++ labcad.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 struct device {
-	char devicename[20];
-	char ipaddress[16];
+	const char *devicename;
+	const char *ipaddress;
 	int deivceid;
-} myDevice, devices[80], *devicePtr;
+} myDevice = {"mivpe015", "10.176.1.39", 1}, devices[80], *devicePtr;
 
 //struct device myDevice;
 //struct device devices[80];
@@ -12,5 +12,14 @@ struct device {
 
 int main() {
   printf("Hello Antonio\n");
+  printf("%s %s %d\n", myDevice.devicename, myDevice.ipaddress, myDevice.deivceid);
+  devicePtr = &myDevice; //assegna l'indirizzo di myDevice a devicePtr
+  printf("%s\n", devicePtr->devicename);
+  printf("%s\n", devicePtr->ipaddress);
+  printf("%d\n", devicePtr->deivceid);
+  myDevice.devicename = "mivpe016";
+  myDevice.ipaddress = "10.176.1.40";
+  myDevice.deivceid = 1;
+  printf("%s %s %d\n", (*devicePtr).devicename, (*devicePtr).ipaddress, (*devicePtr).deivceid);
   return 0;
 }
